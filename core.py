9128d902009421bc14e53f75ef499adeb847d265
MasterPabu <venkat24@outlook.com>
2016-07-17 23:42:53 +0000

    Poster cache clearing and quality adjustment

--- core.py
+++ core.py
@@ -20,13 +20,37 @@ movieList=[]
 movieFolder="/home/venkat/movie_test"
 temp_path=""
 
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','Ki','Mi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+def calc_cache_size():
+    filelist=[f for f in os.listdir("posters/")]
+    size=0
+    for f in filelist:
+        size+=os.path.getsize("posters/"+f)
+    readable_size=str(sizeof_fmt(size))
+    ui.label_3.setText(QtCore.QString("Poster cache takes up "+ str(readable_size)))
+
+def change_poster_quality(ui):
+    quality=str(ui.comboBox.currentText())
+    switcher = {
+        "Low" : "w154",
+        "Medium" : "w300",
+        "High (Default)" : "w500",
+        "Massive" : "original"
+    }
+    global max_size
+    max_size=switcher[quality]
+
 def get_path(ui):
     file_path=QtGui.QFileDialog.getExistingDirectory()
     global temp_path
     temp_path=file_path
     display_path=QtCore.QString(temp_path)
     ui.lineEdit.setText(display_path)
-    print temp_path
 def imdb_id_from_title(title):
     pattern = 'http://www.imdb.com/xml/find?json=1&nr=1&tt=on&q={movie_title}'
     url = pattern.format(movie_title=urllib.quote(title))
@@ -43,12 +67,18 @@ def set_path(ui):
     else:
         display_path=str(ui.lineEdit.text())
         movieFolder = display_path
+        displayList()
+def clear_poster_cache():
+    filelist=[f for f in os.listdir("posters/")]
+    for f in filelist:
+        os.remove("posters/"+f)
+    calc_cache_size()
 def getMovieList():
     global movieFolder
     file_list=[]
     extensions = [
         '.avi',
-        '.mkv',        
+        '.mkv',
         '.mp4',
     ]
     for root, directories, filenames in os.walk(movieFolder):
@@ -74,7 +104,8 @@ def set_movie(ui):
     if "quality" in movieInfo.keys():
         res_print_text_list.append(movieInfo["quality"])
     resPrintText=' '.join(res_print_text_list)
-    ui.lineEdit_3.setText(QtCore.QString("Year - " + str(movieInfo["year"])))
+    if "year" in movieInfo.keys():
+        ui.lineEdit_3.setText(QtCore.QString("Year - " + str(movieInfo["year"])))
     ui.lineEdit_2.setText(QtCore.QString(resPrintText))
     #Poster Code
     if "imdbid" not in movieInfo.keys():
@@ -82,7 +113,7 @@ def set_movie(ui):
         movieInfo["imdbid"]=imdb_id
     else:
         imdb_id=movieInfo["imdbid"]
-    if not os.path.isfile("posters/poster_" + imdb_id + ".jpeg"):
+    if not os.path.isfile("posters/poster_" + imdb_id + "_" + max_size + ".jpeg"):
         ui.label_2.setText("Downloading...")
         image_req = 'http://api.themoviedb.org/3/movie/{imdbid}/images?api_key={key}' 
         r = requests.get(image_req.format(key=api_key,imdbid=imdb_id))
@@ -96,11 +127,14 @@ def set_movie(ui):
         url=poster_urls[0]
         r = requests.get(url)
         filetype = r.headers['content-type'].split('/')[-1]
-        filename = 'posters/poster_{0}.{1}'.format(imdb_id,filetype) 
+        filename = 'posters/poster_{0}_{1}.{2}'.format(imdb_id,max_size,filetype) 
         with open(filename,'wb') as w:
             w.write(r.content)
-    ui.label_2.setPixmap(QtGui.QPixmap(os.getcwd() + "/posters/" + "poster_" + imdb_id + '.jpeg'))    
-def displayList(mode="display",search_string=""):
+    ui.label_2.setPixmap(QtGui.QPixmap(os.getcwd() + "/posters/poster_" + imdb_id + "_" + max_size + '.jpeg'))
+    calc_cache_size()
+
+def displayList(mode="display"):
+    search_string=str(ui.lineEdit_5.text()).lower()
     movies=getMovieList()
     if mode=="search" and search_string=="":
         mode="display"
@@ -118,8 +152,6 @@ def displayList(mode="display",search_string=""):
         else:
             ui.listView.addItem(item)
 
-def printer():
-    print "Yo mama"
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     Form = QtGui.QWidget()
@@ -129,8 +161,12 @@ if __name__ == "__main__":
     ui.pushButton_5.clicked.connect(displayList)
     ui.pushButton_2.clicked.connect(lambda: get_path(ui))
     ui.toolButton.clicked.connect(lambda: set_path(ui))
-    ui.pushButton.clicked.connect(lambda: displayList(mode="search",search_string=str(ui.lineEdit_5.text).lower()))
+    ui.lineEdit_5.textChanged.connect(lambda: displayList(mode="search"))
     ui.listView.itemSelectionChanged.connect(lambda: set_movie(ui))
+    ui.pushButton.clicked.connect(lambda: change_poster_quality(ui))
+    ui.pushButton_3.clicked.connect(lambda: clear_poster_cache())
+    ui.pushButton_4.clicked.connect(lambda: calc_cache_size())
+    calc_cache_size()
     Form.show()
     displayList()
     sys.exit(app.exec_())
